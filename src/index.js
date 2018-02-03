9ec7d1653bd6e407ab5c1550e4f56af7518ce1b1
Scott Weldon <open-source@scott-weldon.com>
2018-02-03 14:51:38 -0800

    Make indentation consistent

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { CipherString } from '../jslib/dist/es/models/domain/cipherString';
 import { SymmetricCryptoKey } from '../jslib/dist/es/models/domain/symmetricCryptoKey'
 
 class StorageService {
-  constructor(){
-    this.data = {}
-  }
-  get (item){
-    return this.data[item]
-  }
-  save (key, value){
-    return this.data[key] = value
-  }
+    constructor(){
+        this.data = {}
+    }
+    get (item){
+        return this.data[item]
+    }
+    save (key, value){
+        return this.data[key] = value
+    }
 }
 
 
@@ -23,17 +23,17 @@ class BitwardenClient{
             url = this.settings[`${subdomain}BaseUrl`] + url
             console.log(url)
             const options = {
-              ...this.options,
-              method
+                ...this.options,
+                method
             }
 
             const _request = new Request(url, options)
             fetch(_request, {
                 body: params
             }).then((response)=>{
-              resolve(response.json())
+                resolve(response.json())
             }).catch((error)=>{
-              console.log(error)
+                console.log(error)
             })
         })
     }
@@ -52,19 +52,19 @@ class BitwardenClient{
     }
 
     static crypto(){
-      return this.thecryptoservice = this.thecryptoservice || new cryptoservice.CryptoService(new StorageService(), new StorageService())
+        return this.thecryptoservice = this.thecryptoservice || new cryptoservice.CryptoService(new StorageService(), new StorageService())
     }
     static encrypt(){
     }
     static decrypt(item){
-      item = new CipherString(item)
+        item = new CipherString(item)
 
-      const [encType, key] = this.settings.key.split('.')
-      this.crypto().rsaDecrypt(key).then((result)=>{
-        console.log('result of rsa Decrypt', result)
-      })
-      const cryptoKey = new SymmetricCryptoKey(key, null, parseInt(encType))
-      return this.crypto().decrypt(item, cryptoKey)
+        const [encType, key] = this.settings.key.split('.')
+        this.crypto().rsaDecrypt(key).then((result)=>{
+            console.log('result of rsa Decrypt', result)
+        })
+        const cryptoKey = new SymmetricCryptoKey(key, null, parseInt(encType))
+        return this.crypto().decrypt(item, cryptoKey)
     }
     static login(email, password){
         // Login to your Bitwarden Account
@@ -74,33 +74,33 @@ class BitwardenClient{
         const service = this.crypto()
         const key = service.makeKey(password, email)
         return service.hashPassword(password, key).then((hashedPassword)=>{
-          const request = {
-            email: email,
-            masterPasswordHash: hashedPassword,
-            provider: null,
-            token: null,
-            remember: null,
-            device: null
-          }
+            const request = {
+                email: email,
+                masterPasswordHash: hashedPassword,
+                provider: null,
+                token: null,
+                remember: null,
+                device: null
+            }
 
-          const formData = new FormData();
-          formData.append('grant_type', 'password')
-          formData.append('username', email)
-          formData.append('password', hashedPassword)
-          formData.append('scope', 'api offline_access')
-          formData.append('client_id', 'web')
+            const formData = new FormData();
+            formData.append('grant_type', 'password')
+            formData.append('username', email)
+            formData.append('password', hashedPassword)
+            formData.append('scope', 'api offline_access')
+            formData.append('client_id', 'web')
 
-          const url = '/connect/token'
+            const url = '/connect/token'
 
-          const response = this.post('identity', url, formData);
+            const response = this.post('identity', url, formData);
 
-          response.then((data)=>{
-            this.setToken(data.access_token)
-            this.setKey(data.Key)
-            this.setPrivateKey(data.PrivateKey)
-            this.setRefreshToken(data.refresh_token)
-          })
-          return response
+            response.then((data)=>{
+                this.setToken(data.access_token)
+                this.setKey(data.Key)
+                this.setPrivateKey(data.PrivateKey)
+                this.setRefreshToken(data.refresh_token)
+            })
+            return response
         })
     }
 
@@ -112,21 +112,21 @@ class BitwardenClient{
         // Log Out of your Bitwarden Account
     }
     static setKey(key){
-      this.settings.key = key
+        this.settings.key = key
     }
     static setToken(token){
-      this.setHeaders({
-        Authorization: `Bearer ${token}`
-      })
+        this.setHeaders({
+            Authorization: `Bearer ${token}`
+        })
     }
     static setRefreshToken(token){
-      this.settings.refreshToken = token
+        this.settings.refreshToken = token
     }
     static setPrivateKey(privateKey){
-      this.settings.privateKey = privateKey
+        this.settings.privateKey = privateKey
     }
     static getPasswords(){
-      return this.get('api', '/ciphers')
+        return this.get('api', '/ciphers')
     }
 
     static setPassword(id, value){
@@ -138,11 +138,11 @@ class BitwardenClient{
     }
 
     static setHeaders(headers = {}){
-      console.log(headers)
-      this.options.headers = new Headers({
-        'Device-Type': 'web',
-        ...headers
-      })
+        console.log(headers)
+        this.options.headers = new Headers({
+            'Device-Type': 'web',
+            ...headers
+        })
     }
 }
 BitwardenClient.settings = {
